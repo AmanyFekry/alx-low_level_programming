aba6bc52ead06f70c9bbf38964bbe648e5380553
AmanyFekry <88165998+AmanyFekry@users.noreply.github.com>
2023-02-21 13:19:56 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * main - check the code
  * @n: digit to find the last place of.
  * @r: An integer input
- * print_last_digit - prints the last digit of a numer.
+ * Description: print_last_digit - prints the last digit of a numer.
  * Return: The last digit
  */
 int print_last_digit(int r)
